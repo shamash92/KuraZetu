a860ec93157f0543ec86e27e3e3a84a4c61f2aed
shamash92 <shamash@tutamail.com>
2025-05-06 21:41:37 +0300

    fix typescript errors (#19)

---
+++ src/ui/src/types/boundaries.ts
@@ -0,0 +1,55 @@
+export interface ICountyBoundary {
+  id: number;
+  type: 'Feature';
+  geometry: {
+    type: 'Polygon';
+    coordinates: [number, number][][];
+  };
+  properties: {
+    number: number;
+    name: string;
+  };
+}
+
+export interface IConstituencyBoundary {
+  id: number;
+  type: 'Feature';
+  geometry: {
+    type: 'Polygon';
+    coordinates: [number, number][][];
+  };
+  properties: {
+    name: string;
+    number: number;
+  };
+}
+
+export interface IWardBoundary {
+  id: number;
+  type: 'Feature';
+  geometry: {
+    type: 'Polygon';
+    coordinates: [number, number][][];
+  };
+  properties: {
+    name: string;
+    number: number;
+  };
+}
+
+export interface IPinLocation {
+  type: 'Point';
+  coordinates: [number, number];
+}
+
+export interface IPollingCenterLocation {
+  id: number;
+  type: 'Feature';
+  geometry: IPinLocation | null;
+  properties: {
+    name: string;
+    code: string;
+    ward: number;
+    is_verified: boolean;
+  };
+}
