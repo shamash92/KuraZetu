55638dedc49b9a575721e8f6b2da8f620f7f6b55
Kovu349 <103192973+Kovu349@users.noreply.github.com>
2026-07-29 15:27:31 -0500

    fix: replace deprecated SafeAreaView in settings layout

--- NATIVE/app/(tabs)/settings/index.tsx
+++ NATIVE/app/(tabs)/settings/index.tsx
@@ -1,6 +1,5 @@
 import {
     Alert,
-    SafeAreaView,
     ScrollView,
     StyleSheet,
     Switch,
@@ -8,6 +7,7 @@ import {
     TouchableOpacity,
     View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context"
 import {
     AlertCircle,
     Bell,
