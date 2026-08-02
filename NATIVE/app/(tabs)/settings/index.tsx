da3ebf5296b78c055e4fcbbe091a2452b4bcc46c
Kovu349 <103192973+Kovu349@users.noreply.github.com>
2026-08-02 15:55:40 -0500

    [native] Replace settings SafeAreaView

--- NATIVE/app/(tabs)/settings/index.tsx
+++ NATIVE/app/(tabs)/settings/index.tsx
@@ -7,7 +7,7 @@ import {
     TouchableOpacity,
     View,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context"
+import { SafeAreaView } from "react-native-safe-area-context";
 import {
     AlertCircle,
     Bell,
